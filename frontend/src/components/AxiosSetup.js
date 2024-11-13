b5bd47c7daa051557afd70e67518cd09d07eaeb7
sashmitha2005 <shrisashmithas.22aid@kongu.edu>
2024-11-13 11:41:59 +0530

    finl

--- frontend/src/components/AxiosSetup.js
+++ frontend/src/components/AxiosSetup.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const AxiosSetup = () =>{
-    axios.defaults.baseURL = 'http://localhost:5000';
+    axios.defaults.baseURL = 'https://typing-speed-rk4s.onrender.com/';
 }
 
 export default AxiosSetup;
